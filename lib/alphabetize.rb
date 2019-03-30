02af1c75564124bf29af5841c4c5af9f01d8f0a7
Marcus Mellor <marcus@mellor.com>
2019-03-30 16:13:08 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,21 @@
+def hash_my_esp( esp_alphabet)
+
+  return_hash = { 
+    "esperanto" => {}
+  }
+  counter=0
+   esp_alphabet.length.times do
+    return_hash["esperanto"][esp_alphabet[counter]]=counter
+    counter+=1
+  end
+  return_hash
+end
+
+my_esp = hash_my_esp(ESPERANTO_ALPHABET)
+
+my_array.sort_by { |l| my_esp["esperanto"][l[0]] }
+
+
 def alphabetize(arr)
   # code here
   
